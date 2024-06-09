9ef5c4e0690865d14e25b80d7c8d6585d61a79e7
Diego <e_guinho@hotmail.com>
2024-06-09 18:47:57 -0300

    update/auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,33 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
-//import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
 
-const poppins = Poppins({ 
+const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
   variable: '--font-poppins',
-});
+})
 
 export const metadata: Metadata = {
-  title: 'Eventsni',
-  description: 'Eventsni é uma plataforma para gerenciamento de eventos.',
+  title: 'Evently',
+  description: 'Evently is a platform for event management.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
-};
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
 }
