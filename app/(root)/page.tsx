87560aea1bdf84dcf0039867505aa93d791e9a61
Diego <e_guinho@hotmail.com>
2024-06-09 13:36:31 -0300

    update/layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <main>
+      <h1 className="text-4xl">Lets Go Party</h1>
       <Button variant="destructive" className="px-10">Button</Button>
     </main>
   );
